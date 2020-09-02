ed02657aebeb9ee3e7f46cb70a3d044fb2001a53
luiscostalafc <luiscostalafc@gmail.com>
2020-09-02 10:06:31 -0300

    Validaçao de login

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -62,6 +62,23 @@ export const Content = styled.div`
        color: ${shade(0.2, '#ff9000')}
      }
  }
+
+ button {
+  background: #ff9000;
+  height: 56px;
+  border-radius: 10px;
+  border: 0;
+  padding: 0 16px;
+  color: #312e38;
+  width: 100%;
+  font-weight: 500;
+  margin-top: 16px;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background: ${shade(0.2, '#ff9000')};
+  }
+}
 `;
 
 export const Background = styled.div`
